fc4912d6aa9f005216afbbcab995621668e4fc1f
hemadri <hemadri.saxena@gmail.com>
2014-10-05 00:15:26 -0400

    char coding

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 <title>Password Generator</title>
+ <meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
 <link rel="stylesheet" type = "text/css" href="style_hemadri.css">	
 <?php require 'logic.php'?>
 </head>
